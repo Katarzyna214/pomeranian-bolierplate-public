ffc886d6bd0e5a183611065d5e322525cd1ecbce
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-06 12:38:23 +0200

    Ukonczone zadania z modulu 6

--- src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
+++ src/App/Exercises/Js/Exercise-js-variables-1/index.jsx
@@ -2,7 +2,8 @@ import './styles.css';
 
 export function Exercise() {
   let string1 = 'Jon Snow';
-
+  const myname = 'Kasia';
+  const num1 = 'piętnastym';
   const string3 = 'Ulubiony bohater z serialu "Gra o tron" to ' + string1;
 
   const string2 = `Ulubiony bohater z serialu "Gra o tron" to: ${string1}`;
@@ -18,8 +19,10 @@ export function Exercise() {
         </p>
         <p>{string2}</p>
         <p>{string3}</p>
-        {/* Uzupełnij zanie korzystając ze zmiennych oraz operacji na stringach */}
-        <p>Witaj ____! Jesteś ____ uzytkownikiem odwiedzającym tę stronę!</p>
+        {/* Uzupełnij zdanie korzystając ze zmiennych oraz operacji na stringach */}
+        <p>
+          Witaj {myname}! Jesteś {num1} użytkownikiem odwiedzającym tę stronę!
+        </p>
       </section>
     </div>
   );
