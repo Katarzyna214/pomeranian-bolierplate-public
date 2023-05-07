2a128a663bd2adb9bba5f45a9a24f7b7c7fb847c
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-07 10:35:20 +0200

    addingbutton

--- src/App/Exercises/ReactExercises/Blok8/Blok8.jsx
+++ src/App/Exercises/ReactExercises/Blok8/Blok8.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+// usestate umozliwia przechowywanie danych
 import './style.css';
 
 export function Blok8() {
-  function clickHandler() {
-    console.log('Kliknięto przycisk');
+  const wartosc = 10;
+
+  const [iloscklikniec, setIlosc] = useState(0);
+  
+  function clickHandler(ev) {
+    console.log('Kliknięto przycisk', ev);
+    setIlosc(iloscklikniec + 1)
+    // alert('Kliknięto przycisk');
+    // alert pokaze mi okienko na przeglądarce nie w konsoli jak to co wyżej
   }
   return (
     <div>
-      <p>Przykład blok 8.</p>
-      <button onClick={clickHandler}>Przycisk</button>
+      {/* <p onClick={clickHandler}>Przykład blok 8.</p>
+      <button onClick={clickHandler} type="button"> Przycisk </button>
+      <p>{wartosc * 2}</p> */}
+      <h2>Cześć!</h2>
+      <button className="btn" onClick={clickHandler} type="button">Kliknięto mnie {iloscklikniec} razy.</button>
     </div>
   );
 }
