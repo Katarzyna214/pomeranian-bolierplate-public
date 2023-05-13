5630c9ee9332da043229f9090b7190f0f7570f10
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-13 13:01:51 +0200

    mojaanimacja

---
+++ src/App/Exercises/HtmlCss/CssAnimationExercise/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { CssAnimationExercise } from './index';
+
+export const blockRouterMetaData = {
+  path: '13-05-2023-CssAnimationExercise',
+  date: '13-12-2023',
+  linkLabel: 'Animacje Css Ćwiczenia',
+  blockNo: 11,
+  element: <CssAnimationExercise />,
+  tags: ['attribute', 'text', 'css', 'animations'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,6 +3,7 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaData12052023 } from './Blok10/router-data';
 import { blockRouterMetaData as blockRouterMetaData133052023 } from './CssAnimation/router-data';
+import { blockRouterMetaData as blockRouterMetaData13305202325 } from './CssAnimationExercise/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, blockRouterMetaData12052023, blockRouterMetaData133052023 ];
+export const blockRouterMetaData = [blockRouterMetaData121220231, blockRouterMetaData12052023, blockRouterMetaData133052023, blockRouterMetaData13305202325 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
