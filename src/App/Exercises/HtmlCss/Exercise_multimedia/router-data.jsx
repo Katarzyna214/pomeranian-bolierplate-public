7f4eaa1b6915433b20681965e1bf5a1fd2bde0a8
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-13 14:11:01 +0200

    roznezapisykolorow

---
+++ src/App/Exercises/HtmlCss/Exercise_multimedia/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Multimedia } from './index';
+
+export const blockRouterMetaData = {
+  path: '13-05-2023-Multimedia',
+  date: '13-12-2023',
+  linkLabel: 'Multimedia-kolory',
+  blockNo: 12,
+  element: <Multimedia />,
+  tags: ['text', 'color', 'multimedia'],
+};
