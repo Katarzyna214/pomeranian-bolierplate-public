81fe3e2fc1a9c4a7ce6d6390dc68dc9339088212
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-13 15:37:40 +0200

    havingfunwithimages

--- src/App/Exercises/HtmlCss/Multimedia_images/index.jsx
+++ src/App/Exercises/HtmlCss/Multimedia_images/index.jsx
@@ -4,9 +4,14 @@ import jpg from '../../../Images/jak-zrobic-dobre-zdjecie-ustawienia-aparatu.jpg
 import gif from '../../../Images/spongebob.gif';
 
 export function Images() {
-  return <div>
-  <img src={png} alt="rower"/>
-  <img src={jpg} alt="krajobraz"/>
-  <img src={gif} alt="spongebob"/>
-</div>;
+  return (
+    <div>
+      <img className="obr1" src={png} alt="rower" />
+      <img src={jpg} alt="krajobraz" />
+      <img src={gif} alt="spongebob" />
+
+      {/* background url */}
+      <div className="imageBackground">test</div>
+    </div>
+  );
 }
