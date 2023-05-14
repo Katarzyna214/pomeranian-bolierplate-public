2b19b8e9c33901096e2486ed9b671a21f5275e98
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-14 10:03:20 +0200

    content_and_border_box_difference

--- src/App/Exercises/HtmlCss/BoxModel/index.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -20,6 +20,17 @@ export function BoxModel() {
         {/* conditional rendering renderuje od nowa a display none nie renderuje od nowa */}
         {isTrue && <span className="inlinespan"> Test inlinowy.</span>}
       </p>
+
+      <p className="paragraf2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+        <span className="inlinespan"> Test inlinowy.</span>
+      </p>
     </div>
   );
 }
