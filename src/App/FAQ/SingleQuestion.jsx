233add0bb0c4c4da8660e9cb82e6232139d11291
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-17 22:45:49 +0200

    another_try

--- src/App/FAQ/SingleQuestion.jsx
+++ src/App/FAQ/SingleQuestion.jsx
@@ -1,21 +1,31 @@
 import './styles.css';
 import React from 'react';
 import { useState } from 'react';
-import {ReactComponent as Vector} from '../Components/Icons/Vector.svg'
+import {ReactComponent as Vector} from '../Components/Icons/Vector.svg';
+
 
 export const SingleQuestion = (props) => {
   const question = props.question;
   const answear = props.answear;
 
   const [isTrue, setIsTrue] = useState(true);
+ 
 
   const clickHandler = () => {
     setIsTrue(!isTrue);
     
     };
- const rotatePick =() => {
-  Vector.transform = 'rotate (45deg)';
- };
+    const clickHandler2 = () => {
+
+      if (clickHandler (!isTrue) ) {
+        Vector.transform = 'rotate (90deg)'; }
+         else {
+          Vector.transform = 'rotate (0deg)';
+      }}
+
+//  const rotatePick = function(icon) {
+//   icon = Vector.transform = 'rotate (90deg)'
+//  };
   return (
     <div className="pierwszybox">
       <h2 className="naglowek2" >
@@ -23,7 +33,7 @@ export const SingleQuestion = (props) => {
       </h2>
       <button onClick={()=> 
       {clickHandler ()
-      rotatePick () }}> <Vector/> </button>
+      clickHandler2 () }}> <Vector/> </button>
       <hr className="hr" />
       <p className="par2">{isTrue && answear}</p>
     </div>
