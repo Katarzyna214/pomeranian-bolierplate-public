7229d66b28cf07f221a91d84865eb0ac18715203
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-20 15:51:36 +0200

    fewchanges

--- src/App/Dashboard/Blocks/Blocks.jsx
+++ src/App/Dashboard/Blocks/Blocks.jsx
@@ -1,14 +1,20 @@
-
-
 export const Blocks = () => {
   return (
     <div className="dashboard-tiles-container">
-      {' '}
-      <h2>Hej Maciej!</h2>{' '}
+      <h2>Hej tu Kasia!</h2>{' '}
       <div className="dashboard-tiles">
         {' '}
-        <div>Moje CV
-          <p className="cvpar">podgląd CV wraz z doświadczeniem</p></div> <div>Projekty</div> <div>Ćwiczenia</div>{' '}
+        <div className="box1">
+          Moje CV
+          <p className="cvpar">podgląd CV wraz z doświadczeniem</p>
+          <p className="cvpar">zobacz więcej </p>
+        </div>{' '}
+        <div>Projekty</div>{' '}
+        <div>
+          Ćwiczenia
+          <p className="cvpar">wszystkie wykonane ćwiczenia</p>
+          <p className="cvpar">zobacz więcej</p>
+        </div>{' '}
         <div>Edukacja</div>{' '}
         <div>
           {' '}
