0b95e0251367b4b4f28132538eacc180f6f92dd0
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-05-20 15:00:57 +0200

    right_corner_menu

---
+++ src/App/Components/RighCornerMenuPopup/RightCornerMenuPopup.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const RightCornerMenuPopup = () => {
+  return (
+    <div className="rightmenu">
+      <div className="login">ZALOGUJ SIĘ</div>
+      <div className="instruction">
+        Nie masz konta? <span>Zarejestruj się</span>
+      </div>
+    </div>
+  );
+};
