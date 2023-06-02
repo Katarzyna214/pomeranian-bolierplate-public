eea2783fe9846b77d401dac308251348f6e8d638
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-02 23:07:00 +0200

    molegamechanges

--- src/App/Exercises/Js/HitTheMoleGame/GameView/GameView.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/GameView/GameView.jsx
@@ -1,8 +1,9 @@
 import './GameView.css';
 import { Button } from '../Button/Button';
 import { Menu } from '../Menu/Menu';
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 
-export const GameView = ({}) => {
+export const GameView = ({ setGameStarted, score, setScore }) => {
   return (
     <>
       <Menu label="CZAS DO KOŃCA">
@@ -10,11 +11,18 @@ export const GameView = ({}) => {
       </Menu>
 
       <Menu label="WYNIK">
-        <div className="value-field">{2}</div>
+        <div className="value-field">{score}</div>
       </Menu>
 
       <Menu label="PRZYCISKI STREUJĄCE">
-        <Button onClick={() => {}}>Stop</Button>
+        <Button
+          onClick={() => {
+            setGameStarted(false);
+            setScore(0);
+          }}
+        >
+          Stop
+        </Button>
       </Menu>
     </>
   );
--- src/App/Exercises/Js/HitTheMoleGame/index.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/index.jsx
@@ -3,8 +3,12 @@ import { Menu } from './Menu/Menu';
 import { Button } from './Button/Button';
 import { GameView } from './GameView/GameView';
 import { PlayGround } from './PlayGround/PlayGround';
+import { useState } from 'react';
 
 export function HitTheMole() {
+  const [isGameStarted, setGameStarted] = useState(false);
+  const [score, setScore] = useState(0);
+
   return (
     <div className="main_hit_the_mole">
       <h4>HIT THE MOLE GAME</h4>
@@ -12,23 +16,34 @@ export function HitTheMole() {
         Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w
         którym się pojawił.{' '}
       </p>
+      {isGameStarted ? (
+        <GameView
+          setGameStarted={setGameStarted}
+          score={score}
+          setScore={setScore}
+        />
+      ) : (
+        <>
+          <Menu label="CZAS GRY">
+            <Button isActive={true}>1 minuta</Button>
+            <Button>2 minuty</Button>
+            <Button>3 minuty</Button>
+          </Menu>
+          <Menu label="LICZBA KRETÓW">
+            <Button isActive>1 kret</Button>
+            <Button>2 krety</Button>
+            <Button>3 krety</Button>
+          </Menu>
 
-      <Menu label="CZAS GRY">
-        <Button isActive={true}>1 minuta</Button>
-        <Button>2 minuty</Button>
-        <Button>3 minuty</Button>
-      </Menu>
-      <Menu label="LICZBA KRETÓW">
-        <Button isActive>1 kret</Button>
-        <Button>2 krety</Button>
-        <Button>3 krety</Button>
-      </Menu>
+          <Menu label="PRZYCISKI STERUJĄCE">
+            <Button onClick={() => setGameStarted(true)}>START</Button>
+          </Menu>
+        </>
+      )}
 
-      <Menu label="PRZYCISKI STERUJĄCE">
-        <Button>START</Button>
-      </Menu>
-      <GameView />
-      <PlayGround />
+      {/* <ResultView result={result} /> */}
+
+      {isGameStarted && <PlayGround score={score} setScore={setScore} />}
     </div>
   );
 }
