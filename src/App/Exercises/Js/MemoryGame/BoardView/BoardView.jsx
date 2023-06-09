9795bd346ac31cf1f7cb58d5ebd309e74a11a957
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-09 21:14:43 +0200

    funkcjonalnoscmemory

--- src/App/Exercises/Js/MemoryGame/BoardView/BoardView.jsx
+++ src/App/Exercises/Js/MemoryGame/BoardView/BoardView.jsx
@@ -1,112 +1,100 @@
 import './BoardView.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const boardFields = [
-  {
-    id: 1,
-    hasClicked: false,
-    label: 'A',
-  },
-  {
-    id: 2,
-    hasClicked: false,
-    label: '2',
-  },
-  {
-    id: 3,
-    hasClicked: false,
-    label: 'R',
-  },
-  {
-    id: 4,
-    hasClicked: false,
-    label: '8',
-  },
-  {
-    id: 5,
-    hasClicked: false,
-    label: 'U',
-  },
-  {
-    id: 6,
-    hasClicked: false,
-    label: 'K',
-  },
-  {
-    id: 7,
-    hasClicked: false,
-    label: '2',
-  },
-  {
-    id: 8,
-    hasClicked: false,
-    label: 'Z',
-  },
-  {
-    id: 9,
-    hasClicked: false,
-    label: 'K',
-  },
-  {
-    id: 10,
-    hasClicked: false,
-    label: '8',
-  },
-  {
-    id: 11,
-    hasClicked: false,
-    label: 'A',
-  },
-  {
-    id: 12,
-    hasClicked: false,
-    label: '4',
-  },
-  {
-    id: 13,
-    hasClicked: false,
-    label: 'R',
-  },
-  {
-    id: 14,
-    hasClicked: false,
-    label: '4',
-  },
-  {
-    id: 15,
-    hasClicked: false,
-    label: 'Z',
-  },
-  {
-    id: 16,
-    hasClicked: false,
-    label: 'U',
-  },
-];
-export const BoardView = ({}) => {
-  const [cards, setCards] = useState(boardFields);
+function shuffleArray(s) {
+  for (let i = s.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [s[i], s[j]] = [s[j], s[i]];
+  }
+  return s;
+}
 
-  const handleClick = (boardFieldClicked) => {
-    setCards(
-      cards.map((card) => {
-        return {
-          ...card,
-          hasClicked: card.id === boardFieldClicked.id,
-        };
-      })
-    );
+const alpha = Array.from(Array(26)).map((e, i) => i + 65); //listy na bazie liczby
+const alphabet = alpha.map((x) => String.fromCharCode(x));
+const getRandomLetters = (amount) => {
+  const shuffled = [...alphabet].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, amount);
+};
+
+function generateBoard(size) {
+  const randomLetters = getRandomLetters(size / 2); //bo polowa liter zawsze odkrywane dwie karty na raz
+  const letters = randomLetters.map((letter) => {
+    return {
+      id: null,
+      value: letter,
+      isPaired: false,
+    };
+  });
+  const mergedLetters = [...letters, ...letters];
+  return shuffleArray(mergedLetters).map((obj, index) => {
+    return { ...obj, id: index + 1 };
+  });
+}
+export const BoardView = ({ boardSize }) => {
+  const [firstClickedFieldId, setFirstClickedFieldId] = useState();
+  const [secondClickedFieldId, setSecondClickedFieldId] = useState();
+  const [board, setBoard] = useState(generateBoard(16));
+  const handleClick = (object) => {
+    const isFirstClickedSetAndIsDifferentThanPrev =
+      firstClickedFieldId && firstClickedFieldId != object.id;
+    if (isFirstClickedSetAndIsDifferentThanPrev) {
+      setSecondClickedFieldId(object.id);
+      resetSecondClickedFieldId();
+    } else {
+      setFirstClickedFieldId(object.id);
+      resetFirstClickedFieldId();
+    }
   };
+
+  const resetFirstClickedFieldId = () => {
+    setTimeout(() => {
+      setFirstClickedFieldId(undefined);
+    }, 5000);
+  };
+  const resetSecondClickedFieldId = () => {
+    setTimeout(() => {
+      setSecondClickedFieldId(undefined);
+    }, 1000);
+  };
+  useEffect(() => {
+    if (firstClickedFieldId && secondClickedFieldId) {
+      const firstClickedFieldValue = board.find(
+        (item) => item.id === firstClickedFieldId
+      );
+      const secondClickedFieldValue = board.find(
+        (item) => item.id === secondClickedFieldId
+      );
+      if (firstClickedFieldValue === secondClickedFieldValue) {
+        setBoard(
+          board.map((field) => {
+            const isClickedFieldPaired =
+              field.id === firstClickedFieldId ||
+              field.id === secondClickedFieldId;
+            return {
+              ...field,
+              isPaired: field.isPaired ? true : isClickedFieldPaired,
+            };
+          })
+        );
+      }
+    }
+  }, [firstClickedFieldId, secondClickedFieldId]);
+  console.log(board);
   return (
     <div className="board_view_main_field">
-      {cards.map((card) => {
+      {board.map((element) => {
+        const isClicked =
+          firstClickedFieldId === element.id ||
+          secondClickedFieldId === element.id;
+        const canShowValue = isClicked || element.isPaired;
+        const clickedFliedClassName = isClicked ? 'field-clicked' : '';
+        const pairedFieldClassName = element.isPaired ? 'green-field' : '';
         return (
           <div
-            className={`field ${card.hasClicked ? `green-field` : ''}`}
-            key={card.id}
-            onClick={() => handleClick(card)}
+            className={`field ${clickedFliedClassName} ${pairedFieldClassName}`}
+            onClick={() => handleClick(element)}
           >
-            {' '}
-            {card.hasClicked ? card.label : ''}
+            {canShowValue && element.value}
           </div>
         );
       })}
