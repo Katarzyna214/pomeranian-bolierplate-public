ac0fa48aec5faca82a1efb0e16f2c0f481ae4758
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-11 13:10:16 +0200

    localstorageexercises

--- src/App/Exercises/Js/SavedInput/index.jsx
+++ src/App/Exercises/Js/SavedInput/index.jsx
@@ -1,23 +1,70 @@
 import './styles.css';
-import {React}  from 'react';
+import { React } from 'react';
+import { useState, useEffect } from 'react';
 
 export function SavedInput() {
- const table = [
-  {
-    nick: 'Janek',
-    id: 1,
-    button: <button/>,
-  },
-  {
-    nick: 'Zenek',
-    id: 2,
-    button: <button/>,
-  }
- ]
-    return (
-      <div>
-      <div className='nick'>NICK </div>
-      <input type="text" className='input-nick'/>
-      <button className='button'>DODAJ</button>
+  const [table, setTable] = useState([]);
+  const [idCounter, setIdCounter] = useState('');
+  const [notification, setNotification] = useState('');
+
+  useEffect(() => {
+    const newTableNamestoLocalStorage =
+      JSON.parse(localStorage.getItem('list-nicks')) || [];
+    setTable(newTableNamestoLocalStorage);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('list-nicks', JSON.stringify(table));
+  }, [table]);
+
+  const addName = () => {
+    if (idCounter.trim() !== '') {
+      if (!table.includes(idCounter.trim())) {
+        setTable([...table, idCounter.trim()]);
+        setIdCounter('');
+        setNotification('Your nick added to the list.');
+        setTimeout(() => {
+          setNotification('');
+        }, 2000);
+      } else {
+        setNotification('You cannot provide the same nick.');
+        setTimeout(() => {
+          setNotification('');
+        }, 2000);
+      }
+    }
+  };
+  const deleteName = (index) => {
+    const newTableofNames = [...table];
+    newTableofNames.splice(index, 1);
+    setTable(newTableofNames);
+  };
+  return (
+    <div>
+      <div className="SavedInput">
+        <h3 className="nick">NICK </h3>
+        <input
+          type="text"
+          className="input-nick"
+          placeholder="Provide your nick."
+          value={idCounter}
+          onChange={(e) => setIdCounter(e.target.value)}
+        />
+        <button className="button" onClick={addName}>
+          DODAJ
+        </button>
+        <p>{notification}</p>
+        <div className="table-container">
+          {table.map((person, id, index) => {
+            return (
+              <div key={id} className="table">
+                {person}
+                <button onClick={() => deleteName(index)}>X</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-)}
+    </div>
+  );
+}
