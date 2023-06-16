d0c13244a8cb6cc225e8d79d1a026f45b47bf8f9
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-16 18:02:11 +0200

    useeffecttheory

---
+++ src/App/Exercises/Js/Exercise-js-promises/router-data.jsx
@@ -0,0 +1,10 @@
+import { JsPromisesExercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'promises-and-me',
+  date: '16-06-2023',
+  linkLabel: 'Promisy w javascripcie',
+  blockNo: 34,
+  element: <JsPromisesExercise />,
+  tags: ['promises', 'asynchronous', 'javascript'],
+};
