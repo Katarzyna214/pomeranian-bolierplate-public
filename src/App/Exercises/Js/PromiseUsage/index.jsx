ebf80539b02dd1f4dc7f0fcf1568ce8149f1cfea
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-17 11:18:48 +0200

    promiseusage1

--- src/App/Exercises/Js/PromiseUsage/index.jsx
+++ src/App/Exercises/Js/PromiseUsage/index.jsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import './styles.css';
+import { useState } from 'react';
 
 export function PromiseUsage() {
+  const [data, setData] = useState();
+  const [category, setCategory] = useState();
   const getJokes = async () => {
     //daje async zeby pokazac ze oczekuje na promisa na jakies info z serwera
     try {
@@ -8,17 +12,21 @@ export function PromiseUsage() {
       console.log('Response before .json():', response);
       const data = await response.json(); //informacje ktore mi przyjda z api musze przerobic na json zebym mogla wyswietlic to na stronie
       console.log('Response after .json()', data);
-      const joke = data.value;
-      console.log(`Heres a joke about ${joke}`);
+      const joke = data;
+
+      setData(joke);
     } catch (err) {
       console.log(err, 'Error');
     }
   };
+  useEffect(() => {
+    getJokes();
+  }, []);
   return (
     <div>
-      {' '}
       <h2>Promise Usage</h2>
-      <button onClick={() => getJokes('')}> Click on me </button>
+      {/* <button onClick={() => getJokes('')}> Get Data! </button> */}
+      <p>{data?.value}</p>
     </div>
   );
 }
