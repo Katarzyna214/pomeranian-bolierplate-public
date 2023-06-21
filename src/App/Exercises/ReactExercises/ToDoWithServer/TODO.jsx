617342776aaa87c25e4e3ce85b7efe008de54d30
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-21 23:51:54 +0200

    button change

--- src/App/Exercises/ReactExercises/ToDoWithServer/TODO.jsx
+++ src/App/Exercises/ReactExercises/ToDoWithServer/TODO.jsx
@@ -20,14 +20,10 @@ export const ToDoElement = ({
             <p className="boxpar">{title}</p>
             <p className="name_date">{author}</p>
             <p className="parinfo">{note}</p>
-            {isError && <p>Nie da się usunąć elementu</p>}
-            <button onClick={() => deleteToDo(id)}>Usuń element</button>
-            {/* <div className="lastdiv_div">
-              <div>
-                <TickIcon />
-              </div>{' '}
-              <div className="name_date">20.04.2024, 18:06</div>
-            </div> */}
+            {isError && <p className="delpar">nie udało się usunąć</p>}
+            <button className="delelement" onClick={() => deleteToDo(id)}>
+              X
+            </button>
           </div>
         </div>
       </div>
