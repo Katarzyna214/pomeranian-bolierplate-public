a344bc4e9672701cbc7ec4e0b8f0604ae617cc8f
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-30 21:04:10 +0200

    formlibrary

---
+++ src/App/Exercises/ReactExercises/BasicForms/router-data.jsx
@@ -0,0 +1,10 @@
+import { BasicForms } from './BasicForms';
+
+export const routerMetaData = {
+  path: 'basic-forms',
+  date: '30-06-2023',
+  linkLabel: 'Formularz zakupowy',
+  blockNo: 42,
+  element: <BasicForms />,
+  tags: ['forms', 'form', 'formularz', 'input', 'checkbox', 'textarea'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -4,10 +4,12 @@ import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Blok8MetaData } from './Blok8/router-data';
 import { Blok9MetaData } from './Blok9/router-data';
 import { routerMetaData as FormsRouterMetaData } from './Forms/router-data';
+import { routerMetaData as BasicFormsRouterMetaData } from './BasicForms/router-data';
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   Blok8MetaData,
   Blok9MetaData,
   FormsRouterMetaData,
+  BasicFormsRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
