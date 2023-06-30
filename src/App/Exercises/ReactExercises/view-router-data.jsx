be9549b5c3b92cfaf9475e6176e3f2b54a83c348
KatarzynaMazur <mazur.katarzya@gmail.com>
2023-06-30 19:00:42 +0200

    formularzHTML

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,5 +3,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Blok8MetaData } from './Blok8/router-data';
 import { Blok9MetaData } from './Blok9/router-data';
-export const blockRouterMetaData = [SubRouteExampleMetaData, Blok8MetaData, Blok9MetaData];
+import { routerMetaData as FormsRouterMetaData } from './Forms/router-data';
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  Blok8MetaData,
+  Blok9MetaData,
+  FormsRouterMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
